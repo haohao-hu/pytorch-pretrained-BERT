b15b6e32772d40843da8b9c77332278139b880f7
Haohao HU <s1107428@s.eduhk.hk>
2019-01-27 10:20:18 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,11 +15,12 @@ python rdc_classification.py \
   --data_dir rdc_dataset/ \
   --bert_model bert-large-uncased \
   --max_seq_length 70 \
-  --train_batch_size 128 \
+  --train_batch_size 256 \
   --eval_batch_size 800 \
   --learning_rate 2.5e-4 \
   --num_train_epochs 5.0 \
-  --output_dir models/rdcd_output/ > /artifacts/e5-lr2.5e-4-bs128.log
+  --output_dir models/rdcd_output/ 
+  #> /artifacts/e5-lr2.5e-4-bs128.log
 
   #--fp16 \   
   #--loss_scale 128 > /artifacts/e5-lr2.5e-4-bs32.log
