6a3368114c9824357de434d9a4250880a1e64d86
Haohao HU <s1107428@s.eduhk.hk>
2019-01-28 12:15:17 -0500

    Update run_lm_fine_tune.sh

--- run_lm_fine_tune.sh
+++ run_lm_fine_tune.sh
@@ -8,16 +8,18 @@ pip install --editable .
 #git clone https://github.com/NVIDIA/apex.git && cd apex && python setup.py install --cuda_ext --cpp_ext
 #cd ..
 
-python run_lm_finetuning_rdcd.py \
+python /storage/pytorch-pretrained-BERT/run_lm_finetuning_rdcd.py \
   --bert_model bert-large-uncased \
   --do_train \
   --do_lower_case \
   --train_file /storage/pytorch-pretrained-BERT/rdc_whole_set.txt \
-  --output_dir /storage/pytorch-pretrained-BERT/models/rdcd_fine_tune_lm_lr6e-5_e5_bs64 \
+  --output_dir /storage/pytorch-pretrained-BERT/models/rdcd_fine_tune_lm_no_truncate_testing \
   --num_train_epochs 5.0 \
   --learning_rate 6e-5 \
   --train_batch_size 64 \
-  --max_seq_length 164 #> /artifacts/e5-lr3e-5-bs64-bert-lm.log
+  --no_truncate \
+  --max_seq_length 108 
+  #--max_seq_length 164 #> /artifacts/e5-lr3e-5-bs64-bert-lm.log
   #--no_truncate \
   #--fp16 \   
   #--loss_scale 128 > /artifacts/e5-lr2.5e-4-bs32.log
