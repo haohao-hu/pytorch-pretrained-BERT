41217dcc20fadb34c841ab54a29a5c092b571d2e
Haohao HU <s1107428@s.eduhk.hk>
2019-01-28 23:45:00 -0500

    Update run_lm_fine_tune.sh

--- run_lm_fine_tune.sh
+++ run_lm_fine_tune.sh
@@ -15,12 +15,12 @@ python run_lm_finetuning_rdcd.py \
   --do_train \
   --do_lower_case \
   --train_file /storage/pytorch-pretrained-BERT/rdc_whole_set_with_unknown_category_in_va_te.tsv \
-  --output_dir /storage/pytorch-pretrained-BERT/models/rdcd_fine_tune_lm_no_truncate_testing_82 \
+  --output_dir /storage/pytorch-pretrained-BERT/models/rdcd_fine_tune_lm_no_truncate_testing_83 \
   --num_train_epochs 5.0 \
   --learning_rate 5e-5 \
-  --train_batch_size 80 \
+  --train_batch_size 64 \
   --no_truncate \
-  --max_seq_length 200 \
+  --max_seq_length 325 \
   --classes_file /storage/pytorch-pretrained-BERT/rdc_dataset/classes.txt \
   --with_category
   #--max_seq_length 164 #> /artifacts/e5-lr3e-5-bs64-bert-lm.log
