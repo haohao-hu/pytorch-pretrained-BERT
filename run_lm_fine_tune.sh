4bdc058cb63ccb379a36d050ec13455fc5e5ab6c
Haohao HU <s1107428@s.eduhk.hk>
2019-01-30 20:30:21 -0500

    Update run_lm_fine_tune.sh

--- run_lm_fine_tune.sh
+++ run_lm_fine_tune.sh
@@ -22,7 +22,7 @@ python run_lm_finetuning_rdcd.py \
   --max_seq_length 325 \
   --classes_file /storage/pytorch-pretrained-BERT/rdc_dataset/classes.txt \
   --on_memory \
-  --with_category  > /artifacts/e5-lr3e-5-bs32-msl325-bert-lm-lg.log
+  --with_category  > /artifacts/e5-lr3e-5-bs32-msl325-bert-lm-lg-2.log
   #--max_seq_length 164 #> /artifacts/e5-lr3e-5-bs64-bert-lm.log
   #--no_truncate \ --no_truncate \
   #--fp16 \   
