d1e2626421803dffe2d718773cefdfb341879ae5
Haohao HU <s1107428@s.eduhk.hk>
2019-01-31 21:58:43 -0500

    Update run_lm_fine_tune.sh

--- run_lm_fine_tune.sh
+++ run_lm_fine_tune.sh
@@ -9,24 +9,25 @@ pip install tensorflow torch torchvision scipy numpy
 #pip install pytorch-pretrained-bert
 #git clone https://github.com/NVIDIA/apex.git && cd apex && python setup.py install --cuda_ext --cpp_ext
 #cd ..
-
+tensorboard --logdir='/storage/pytorch-pretrained-BERT/logs' --port=8888:8888
 #python /storage/pytorch-pretrained-BERT/run_lm_finetuning_rdcd.py \
 python run_lm_finetuning_rdcd.py \
   --bert_model bert-large-uncased \
   --do_train \
   --do_lower_case \
   --train_file /storage/pytorch-pretrained-BERT/rdc_dataset/train.tsv \
-  --output_dir /storage/pytorch-pretrained-BERT/models/rdcd_fine_tune_lm_testing_train600k_4 \
-  --num_train_epochs 5.0 \
-  --learning_rate 3.5e-5 \
+  --output_dir /storage/pytorch-pretrained-BERT/models/rdcd_fine_tune_lm_testing_train600k_find_lr \
+  --num_train_epochs 0.1 \
+  --learning_rate 1e-4 \
   --train_batch_size 32 \
   --max_seq_length 256 \
   --classes_file /storage/pytorch-pretrained-BERT/rdc_dataset/classes.txt \
   --on_memory \
+  --warmup_proportion 1 \
   --truncate_length 20 \
   --with_category  
   
-tensorboard --logdir='/storage/pytorch-pretrained-BERT/logs' --port=8888:8888  > /artifacts/e5-lr3.5e-5-bs32-msl256-bert-lm-lg-train600k.log
+  #> /artifacts/e5-lr3.5e-5-bs32-msl256-bert-lm-lg-train600k.log
   #--max_seq_length 164 #> /artifacts/e5-lr3e-5-bs64-bert-lm.log
   #--no_truncate \ --no_truncate \
   #--fp16 \   
