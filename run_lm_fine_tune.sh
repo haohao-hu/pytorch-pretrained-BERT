1e4fbdf6096fd3fa9acef54c21c6f2ac58f6290b
Haohao HU <s1107428@s.eduhk.hk>
2019-01-31 22:04:58 -0500

    Update run_lm_fine_tune.sh

--- run_lm_fine_tune.sh
+++ run_lm_fine_tune.sh
@@ -9,14 +9,14 @@ pip install tensorflow torch torchvision scipy numpy
 #pip install pytorch-pretrained-bert
 #git clone https://github.com/NVIDIA/apex.git && cd apex && python setup.py install --cuda_ext --cpp_ext
 #cd ..
-tensorboard --logdir='/storage/pytorch-pretrained-BERT/logs' --port=8888:8888
+tensorboard --logdir='/storage/pytorch-pretrained-BERT/logs' --port=8888
 #python /storage/pytorch-pretrained-BERT/run_lm_finetuning_rdcd.py \
 python run_lm_finetuning_rdcd.py \
   --bert_model bert-large-uncased \
   --do_train \
   --do_lower_case \
   --train_file /storage/pytorch-pretrained-BERT/rdc_dataset/train.tsv \
-  --output_dir /storage/pytorch-pretrained-BERT/models/rdcd_fine_tune_lm_testing_train600k_find_lr \
+  --output_dir /storage/pytorch-pretrained-BERT/models/rdcd_fine_tune_lm_testing_train600k_find_lr_1 \
   --num_train_epochs 0.1 \
   --learning_rate 1e-4 \
   --train_batch_size 32 \
